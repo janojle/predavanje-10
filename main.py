476a066eab70df7ef7a29f5d2940281ec779e940
janojle <darkojanojlic@hotmail.com>
2024-08-03 11:59:19 +0200

    finished main.py and data.json

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-test
+import  json
+#r - read
+with open("data.json", "r") as file:
+    data = json.load(file)
+    #data i file su varijable
+    data.append({
+        "name": "Petar Petrovic",
+        "age": 29,
+        "height": 187,
+        "gender": "male"
+    })
+
+print(data)
+
+# w -write
+with open("data.json", "w") as file:
+    json.dump(data, file, indent=4)
